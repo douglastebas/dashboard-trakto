228c2d42fc8835ef005343ba1ca6c847b51bc702
Douglas Tebas <dougtebas@gmail.com>
2023-04-24 23:11:09 -0300

    Add bootstrap to project, redirect to Login component and minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ModuleListComponent } from './module-list/module-list.component';
+import { ModuleComponent } from './module/module.component';
+import { DesignListComponent } from './design-list/design-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ModuleListComponent,
+    ModuleComponent,
+    DesignListComponent,
   ],
   imports: [
     BrowserModule,
