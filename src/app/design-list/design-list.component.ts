975ccd08ce909a41915677e527736ee10e937d63
Douglas Tebas <dougtebas@gmail.com>
2023-04-24 23:09:09 -0300

    Create Design list component

---
+++ src/app/design-list/design-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-design-list',
+  templateUrl: './design-list.component.html',
+  styleUrls: ['./design-list.component.scss']
+})
+export class DesignListComponent {
+
+}
