2ad8bcf25931ca8a6591c99b38e022ed67b2b76a
Douglas Tebas <dougtebas@gmail.com>
2023-04-24 23:08:38 -0300

    Create Module list component

---
+++ src/app/module-list/module-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-module-list',
+  templateUrl: './module-list.component.html',
+  styleUrls: ['./module-list.component.scss']
+})
+export class ModuleListComponent {
+
+}
